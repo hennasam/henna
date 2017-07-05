ec44c492a3f1e111128cc795457efdde3aa48f1d
hennasam <hennaedwin1996@gmail.com>
2017-07-05 15:03:44 +0530

    Add files via upload

---
+++ StringToIntegerConversion.java
@@ -0,0 +1,17 @@
+package problems;
+import java.util.Scanner;
+public class StringToIntegerConversion {
+		public static void main(String[] args) {
+	        Scanner sc=new Scanner(System.in);
+	        String str1,str2;
+			System.out.println("Enter a string");
+	        str1=sc.nextLine();
+			str2=sc.nextLine();
+			int a = (Integer.valueOf(str1));
+	        int b = (Integer.valueOf(str2));
+			System.out.println("The SUM is " +(a + b));
+	        System.out.println("Difference is " +(a - b));
+			}
+	}
+
+
