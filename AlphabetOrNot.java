6d59ba636db2bae1fd3a9e447891162fabe1327b
hennasam <hennaedwin1996@gmail.com>
2017-07-11 14:27:31 +0530

    Add files via upload

---
+++ AlphabetOrNot.java
@@ -0,0 +1,24 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class AlphabetOrNot
+{
+    public static void main(String args[])
+    {
+        char ch;
+        Scanner scan = new Scanner(System.in);
+		
+        System.out.print("Enter a Character : ");
+        ch = scan.next().charAt(0);
+		
+        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+        {
+            System.out.print(ch + " is an Alphabet");
+        }
+        else
+        {
+            System.out.print(ch + " is not an Alphabet");
+        }
+    }
+}
